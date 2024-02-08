d830d768d25a5e3f412d492700c6e842ecd4fd58
Ogoa <ogopabhs@gmail.com>
2024-02-08 15:44:59 +0000

    Update: handle kwargs in dunder init of BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -9,12 +9,30 @@ class BaseModel:
     '''Definition of a base class from which other classes will inherit
     '''
 
-    def __init__(self):
-        '''Initialises a new object'''
+    def __init__(self, *args, **kwargs):
+        '''Initialises a new object
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        args: arguments passed for the constructor
+        kwargs: keyword arguments
+        '''
+
+        if kwargs:
+            self.id = str(uuid.uuid4())
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                else:
+                    if key in ["created_at", "updated_at"]:
+                        setattr(self,
+                                key,
+                                datetime.strptime(value,
+                                                  "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def save(self):
         '''Update the attribute 'updated_at' with the current
@@ -31,7 +49,8 @@ class BaseModel:
         obj_dict = dict()
         for key, value in self.__dict__.items():
             if key in ["created_at", "updated_at"]:
-                obj_dict[key] = self.__dict__[key].strftime("%Y-%m-%dT%H:%M:%S.%f")
+                obj_dict[key] = self.__dict__[key].strftime(
+                        "%Y-%m-%dT%H:%M:%S.%f")
             else:
                 obj_dict[key] = value
 
