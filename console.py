f47336d8ede0f3ff0d608ffc7409e170e8ad12fc
Hansel Omondi <87500038+Ogoa@users.noreply.github.com>
2024-02-10 14:33:11 +0000

    Update: include import statements for storage and models
    Corrections were also made to the docstrings of the do_quit() and do_EOf()
    methods.

--- console.py
+++ console.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 """module that defines the command line console"""
 import cmd
+from models import storage
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
-    prompt = "(hbnb)"
+    prompt = "(hbnb) "
+    valid_classes = ["BaseModel"]
 
     def do_quit(self, arg):
-        """Quit command to exit the program"""
+        """Quit command to exit the program\n"""
         return True
 
     def do_EOF(self, arg):
-        """Quit command to exit the program"""
+        """Quit command to exit the program\n"""
         print()
         return True
 
