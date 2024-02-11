6602be942614e682d380ebb3bd86f9964e210bbe
EstherKiguru <esipraisewambui@gmail.com>
2024-02-11 11:34:20 +0300

    new class creeated

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""module for class Amenity"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Inherited State Amenity
+    """
+    name = ""
